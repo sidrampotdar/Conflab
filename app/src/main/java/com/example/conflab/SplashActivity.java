d02c4f3a80a7751360ae9a95c6b23d45a1ea4c84
sidramappapotdar <sidramappapotdar@gmail.com>
2024-01-02 22:56:55 +0530

    SplachActivity Added

---
+++ app/src/main/java/com/example/conflab/SplashActivity.java
@@ -0,0 +1,25 @@
+package com.example.conflab;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import ai.deepar.deepar_example.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+       new  Handler().postDelayed(new Runnable() {
+           @Override
+           public void run() {
+               startActivity(new Intent(SplashActivity.this,MainActivity.class));
+               finish();
+           }
+       },3000);
+    }
+}
