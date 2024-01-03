06d61e4f882b4d9e1493eeb92a0452b55fe910ac
sidramappapotdar <sidramappapotdar@gmail.com>
2024-01-03 17:27:07 +0530

    login activity backend updated

--- app/src/main/java/com/example/conflab/LoginActivity.java
+++ app/src/main/java/com/example/conflab/LoginActivity.java
@@ -1,17 +1,90 @@
 package com.example.conflab;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 import ai.deepar.deepar_example.R;
 
 public class LoginActivity extends AppCompatActivity {
+EditText edtTxtEmail,edtTxtPassword;
+Button btnLonIn;
+TextView txtSignUp;
+FirebaseAuth auth;
+String emailRegex="^[a-zA-Z0-9_+&*-]+(?:\\\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\\\.)+[a-zA-Z]{2,7}$";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        edtTxtEmail=findViewById(R.id.edtTxtLogEmail);
+        edtTxtPassword=findViewById(R.id.edtTxtLogPassword);
+
+        btnLonIn=findViewById(R.id.btnLonIn);
+
+        txtSignUp=findViewById(R.id.txtSignUP);
+
+        auth=FirebaseAuth.getInstance();
+
+        btnLonIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email,password;
+                email=edtTxtEmail.getText().toString();
+                password=edtTxtPassword.getText().toString();
+//                if(email.isEmpty()||password.isEmpty()){
+//                    Toast.makeText(LoginActivity.this,"NULL Values",Toast.LENGTH_LONG);
+//
+//                }
+                if(TextUtils.isEmpty(email)){
+                    Toast.makeText(LoginActivity.this,"Invalid Email",Toast.LENGTH_LONG).show();
+                } else if(TextUtils.isEmpty(password)){
+                    Toast.makeText(LoginActivity.this,"Invalid Password",Toast.LENGTH_LONG).show();
+                }
+              else  if(!email.matches(emailRegex)){
+                  edtTxtEmail.setError("Invalid Email");
+
+                }
+
+                else{
+                    auth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+
+                            if(task.isSuccessful()){
+                                try{
+                                    startActivity(new Intent(LoginActivity.this,MainActivity2.class));
+                                      finish();
+                                } catch (Exception e) {
+                                    Toast.makeText(LoginActivity.this,e.getMessage(),Toast.LENGTH_LONG).show();
+
+                                }
+                            }else{
+                                Toast.makeText(LoginActivity.this,task.getException().getMessage(),Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    });
+
+                }
+            }
+        });
+
+
 
 
     }
--- app/src/main/java/com/example/conflab/RegisterActivity.java
+++ app/src/main/java/com/example/conflab/RegisterActivity.java
@@ -2,15 +2,34 @@ package com.example.conflab;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import ai.deepar.deepar_example.R;
 
 public class RegisterActivity extends AppCompatActivity {
 
+   EditText edtTxtUName,edtTxtREmail,edtTxtRPassword,edtTxtRPassword2;
+   Button btnSignUp;
+   TextView txtLogIn;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        edtTxtUName=findViewById(R.id.edtTxtUName);
+        edtTxtREmail=findViewById(R.id.edtTxtREmail);
+        edtTxtRPassword =findViewById(R.id.edtTxtRPassword);;
+        edtTxtRPassword2=findViewById(R.id.edtTxtPasswordx2);
+
+        btnSignUp=findViewById(R.id.btnSignUp);
+
+        txtLogIn=findViewById(R.id.txtLogin);
+
+
     }
 }
