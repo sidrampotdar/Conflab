42746eb9ba843a3a5c0d08690300ad58d79a9874
sidramappapotdar <sidramappapotdar@gmail.com>
2024-01-04 15:32:09 +0530

    RecyclerView Added with Adapter

--- app/src/main/java/com/example/conflab/MainActivity2.java
+++ app/src/main/java/com/example/conflab/MainActivity2.java
@@ -8,14 +8,121 @@ import android.os.Bundle;
 import com.google.firebase.auth.FirebaseAuth;
 
 import ai.deepar.deepar_example.R;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.annotation.SuppressLint;
+import android.app.Dialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class MainActivity2 extends AppCompatActivity {
 FirebaseAuth auth;
+
+    RecyclerView mainUserRecyclerView;
+    UserAdpter  adapter;
+    FirebaseDatabase database;
+    ArrayList<Users> usersArrayList;
+    ImageView imglogout;
+    ImageView cumbut,setbut;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-        auth=FirebaseAuth.getInstance();
+
+        database=FirebaseDatabase.getInstance();
+        auth = FirebaseAuth.getInstance();
+
+        cumbut = findViewById(R.id.camBut);
+        setbut = findViewById(R.id.settingBut);
+
+        DatabaseReference reference = database.getReference().child("user");
+        usersArrayList = new ArrayList<>();
+
+        mainUserRecyclerView = findViewById(R.id.mainUserRecyclerView);
+        mainUserRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new UserAdpter(MainActivity2.this,usersArrayList);
+        mainUserRecyclerView.setAdapter(adapter);
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot dataSnapshot: snapshot.getChildren())
+                {
+                    Users users = dataSnapshot.getValue(Users.class);
+                    usersArrayList.add(users);
+                }
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+        imglogout = findViewById(R.id.logoutimg);
+
+//        imglogout.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Dialog dialog = new Dialog(MainActivity2.this,R.style.dialoge);
+//                dialog.setContentView(R.layout.dialog_layout);
+//                Button no,yes;
+//                yes = dialog.findViewById(R.id.yesbnt);
+//                no = dialog.findViewById(R.id.nobnt);
+//                yes.setOnClickListener(new View.OnClickListener() {
+//                    @Override
+//                    public void onClick(View v) {
+//                        FirebaseAuth.getInstance().signOut();
+//                        Intent intent = new Intent(MainActivity2.this,LoginActivity.class);
+//                        startActivity(intent);
+//                        finish();
+//                    }
+//                });
+//                no.setOnClickListener(new View.OnClickListener() {
+//                    @Override
+//                    public void onClick(View v) {
+//                        dialog.dismiss();
+//                    }
+//                });
+//                dialog.show();
+//            }
+//        });
+
+//        setbut.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(MainActivity2.this, setting.class);
+//                startActivity(intent);
+//            }
+//        });
+
+        cumbut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            startActivity(new Intent(MainActivity2.this,MainActivity.class));
+            }
+        });
+
+
+
+
         if (auth.getCurrentUser()==null){
             startActivity(new Intent(MainActivity2.this,LoginActivity.class));
             finish();
