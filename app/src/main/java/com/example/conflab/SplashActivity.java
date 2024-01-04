79e189afe4d3ef8c6cee33a73a34944846b18e19
sidramappapotdar <sidramappapotdar@gmail.com>
2024-01-04 09:11:50 +0530

    Anim  added Succefully

--- app/src/main/java/com/example/conflab/SplashActivity.java
+++ app/src/main/java/com/example/conflab/SplashActivity.java
@@ -5,6 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
@@ -25,6 +29,22 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        ImageView logo;
+        TextView name, own1, own2;
+        Animation topAnim, bottomAnim;
+        logo = findViewById(R.id.logoimg);
+        name = findViewById(R.id.logonameimg);
+        own1 = findViewById(R.id.ownone);
+        own2 = findViewById(R.id.owntwo);
+
+        topAnim = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+
+        logo.setAnimation(topAnim);
+        name.setAnimation(bottomAnim);
+        own1.setAnimation(bottomAnim);
+        own2.setAnimation(bottomAnim);
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
